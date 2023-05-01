8fe750295f61b5a087e9d2f25604da7349518722
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-05-01 21:35:05 +0600

    first commit for connecting with git

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVMfZ3KCiy-6ANV_oviv_BL_AtejF7AZg",
+  authDomain: "chef-shop-client.firebaseapp.com",
+  projectId: "chef-shop-client",
+  storageBucket: "chef-shop-client.appspot.com",
+  messagingSenderId: "639371037961",
+  appId: "1:639371037961:web:ab7a9367fc0625a6aed7d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
