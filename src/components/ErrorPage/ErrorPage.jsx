0c6536e741c390f1011bcfdc1b5578851a96e05b
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-05-01 22:24:07 +0600

    router setup initially done

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './ErrorPage.css';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ErrorPage;
