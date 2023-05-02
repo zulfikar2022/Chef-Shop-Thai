ec86862efb06212ea8fbe11bb6fd46032add2d53
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-05-02 12:18:07 +0600

    user creation and add displayName and photoURL successfully implemented

--- src/components/AuthProvider/AuthProvider.jsx
+++ src/components/AuthProvider/AuthProvider.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable no-unused-vars */
-import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { app } from '../../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
+// eslint-disable-next-line react-refresh/only-export-components
 export const auth = getAuth(app);
 
+// eslint-disable-next-line react/prop-types
 const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+
+    const createUser = (email,password) => {
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
 
 
 
-
-    const authInfo = {};
+    const authInfo = {createUser,auth,user,setUser};
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,19 +1,63 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { getAuth, updateProfile } from "firebase/auth";
 
 const Registration = () => {
-    const [accepted,setAccepted] = useState(false);
-    const handleAccepted = (event) => {
-        setAccepted(event.target.checked);
-    }
+  const { createUser,auth,setUser,user } = useContext(AuthContext);
+  
+  const [accepted, setAccepted] = useState(false);
+  const [successMessage,setSuccessMessage] = useState('');
+  const [failureMessage,setFailureMessage] = useState('');
+
+
+  const handleAccepted = (event) => {
+    setAccepted(event.target.checked);
+  };
+
+  const handleRegistration = (event) => {
+    setSuccessMessage('');
+    setFailureMessage('');
+
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const photoUrl = form.photo.value;
+    const password = form.password.value;
+    console.log(name, email, photoUrl, password);
+    createUser(email, password)
+      .then((result) => {
+        const createdUser = result.user;
+        // console.log(createdUser);
+        form.reset();
+        updateProfile(auth.currentUser,{
+          displayName:name,
+          photoURL:photoUrl,
+        });
+        console.log(createdUser);
+        setUser(createdUser);
+        setSuccessMessage('User Successfully Created!!');
+      })
+      .catch((error) => {
+        console.log(error);
+        setFailureMessage(error.message);
+      });
+  };
+
   return (
     <Container className="w-50 mt-3">
-      <Form>
+      <Form onSubmit={handleRegistration}>
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" name="name" placeholder="Enter name" />
+          <Form.Control
+            type="text"
+            required
+            name="name"
+            placeholder="Enter name"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -28,7 +72,12 @@ const Registration = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPhotoUrl">
           <Form.Label>Phot URL</Form.Label>
-          <Form.Control type="text" name="photo" placeholder="Photo url" />
+          <Form.Control
+            type="text"
+            required
+            name="photo"
+            placeholder="Photo url"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -45,19 +94,28 @@ const Registration = () => {
             onClick={handleAccepted}
             type="checkbox"
             name="accept"
-            label={
-              <>
-                Accepts Terms and Conditions
-              </>
-            }
+            label={<>Accepts Terms and Conditions</>}
           />
         </Form.Group>
         {/*this section is for success or failure message  */}
-        <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
-        <Button disabled={!accepted} variant="dark" className="w-100" type="submit">
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          {successMessage && <p className="text-success fw-semibold">{successMessage}</p>}
+          {failureMessage && <p className="text-danger fw-semibold">{failureMessage}</p>}
+        </Form.Group>
+        <Button
+          disabled={!accepted}
+          variant="dark"
+          className="w-100"
+          type="submit"
+        >
           Register
         </Button>
-        <p>Already have an acount? <Link to='/login' className='text-danger text-decoration-underline'>Please Login</Link> </p>
+        <p className="mt-2">
+          Already have an acount?{" "}
+          <Link to="/login" className="text-danger text-decoration-underline">
+            Please Login
+          </Link>{" "}
+        </p>
       </Form>
     </Container>
   );
