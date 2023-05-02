11df1ea80747da6c01aeebfca2985c0725853b31
zulfikar2022 <sayedzulfikar2019@gmail.com>
2023-05-02 17:58:31 +0600

    one extra section that is a gallery added

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Gallery.css';
+import photo1 from '../../assets/food1.jpg';
+import photo2 from '../../assets/food2.jpg';
+import photo3 from '../../assets/food3.jpg';
+import photo4 from '../../assets/food4.jpg';
+import photo5 from '../../assets/food5.jpg';
+import photo6 from '../../assets/food6.jpg';
+
+const Gallery = () => {
+    return (
+        <div className='mt-3'>
+            <h1 className='text-center'>Our Gallery</h1>
+            <div className='gallery-container'>
+                <div>
+                    <img src={photo1} />
+                </div>
+                <div>
+                    <img src={photo2} />
+                </div>
+                <div>
+                    <img src={photo3} />
+                </div>
+                <div>
+                    <img src={photo4} />
+                </div>
+                <div>
+                    <img src={photo5} />
+                </div>
+                <div>
+                    <img src={photo6} />
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Gallery;
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './HomePage.css'
+import Gallery from '../Gallery/Gallery';
+
 
 const HomePage = () => {
     return (
         <div>
-            <h1>This is a HomePage</h1>
+            <Gallery></Gallery>
         </div>
     );
 };
